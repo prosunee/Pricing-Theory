716cf6777e1142b55106813b21ca9c3038ca5b78
Betty Qin <37390962+kllqzh@users.noreply.github.com>
2022-10-15 11:05:44 -0400

    reformatting

--- Main.py
+++ Main.py
@@ -5,18 +5,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-#Exercise Boundaries with varying volatilities and varying risk free interest rates   
 
-    sigmas = [0.1, 0.15, 0.2, 0.25, 0.3]
-    risk_free_rates = [0.005, 0.02, 0.035, 0.05]
-
-    for sigma in sigmas:
-        CRRPricer_A(1, 10, 0.05, sigma, 0.02, 5000, 10)
-    for rate in risk_free_rates:
-        CRRPricer_A(1, 10, 0.05, 0.2, rate, 5000, 10)
-
-
-
+#stimulated profit and loses at various volatility with 20% volatility excercise boundary
     T = 1; S0 = 10; mu = 0.05; sigma =  0.2; r = 0.02; N = 5000; paths = 100; K = 10
     dt = T/N
     crr, exercise_boundary = CRRPricer_A(T, S0, mu, sigma, r, N, K)
@@ -25,16 +15,18 @@ if __name__ == "__main__":
     #profit and loses
     PL = KDE_profit(crr, exercise_boundary, S, K, r, dt)
 
+    #profit and loses plot for 20% volatility
     ax = PL.plot(kind='hist')
     PL.plot(kind='kde', secondary_y= True)
     plt.show()
 
-    #realized volatility 
+    #Stimulate price paths for various realized volatility 
     S10 = pricePaths(T, S0, K, mu, 0.1, r, N, paths)
     S15 = pricePaths(T, S0, K, mu, 0.15, r, N, paths)
     S25 = pricePaths(T, S0, K, mu, 0.25, r, N, paths)
     S30 = pricePaths(T, S0, K, mu, 0.3, r, N, paths)
 
+    #Calculate Profit and Loses with different realized volatility and appling 20% volatility trading strategy
     PL10 = KDE_profit(crr, exercise_boundary, S10, K, r, dt)
     PL15 = KDE_profit(crr, exercise_boundary, S15, K, r, dt)
     PL25 = KDE_profit(crr, exercise_boundary, S25, K, r, dt)
@@ -46,6 +38,17 @@ if __name__ == "__main__":
     df.plot(kind='kde', secondary_y= True)
     plt.show()
 
+'''
+#Exercise Boundaries with varying volatilities and varying risk free interest rates   
+
+    sigmas = [0.1, 0.15, 0.2, 0.25, 0.3]
+    risk_free_rates = [0.005, 0.02, 0.035, 0.05]
+
+    for sigma in sigmas:
+        CRRPricer_A(1, 10, 0.05, sigma, 0.02, 5000, 10)
+    for rate in risk_free_rates:
+        CRRPricer_A(1, 10, 0.05, 0.2, rate, 5000, 10)
+'''
 
 
 
