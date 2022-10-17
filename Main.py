5e3e2853d47360ad7f311c93e6acaf699708d668
Betty Qin <37390962+kllqzh@users.noreply.github.com>
2022-10-17 19:54:53 -0400

    Other parameter changes

--- Main.py
+++ Main.py
@@ -17,15 +17,122 @@ if __name__ == "__main__":
     PL20, PL20_2 = KDE_profit(crr, exercise_boundary, S, K, r, dt)
     T20 = KDE_time(exercise_boundary, S, N)
 
-    """ 
+    #Parameter T 0.5, 2
+    crrt05, exercise_boundaryt05 = CRRPricer_A(0.5, S0, mu, sigma, r, N, K)
+    crrt2, exercise_boundaryt2 = CRRPricer_A(2, S0, mu, sigma, r, N, K)
+    St05 = pricePaths(0.5, S0, K, mu, sigma, r, N, paths)
+    St2 = pricePaths(2, S0, K, mu, sigma, r, N, paths)
+    
+    PLt05, PLt05_2 = KDE_profit(crrt05, exercise_boundaryt05, St05, K, r, 0.5/N)
+    PLt2, PLt2_2 = KDE_profit(crrt2, exercise_boundaryt2, St2, K, r, 2/N)
+
+    '''
+    df = PLt05.merge(PL20,how ='left', on = 'row_num').merge(PLt2,how ='left', on = 'row_num')
+    df.columns = ["row_num",r'T = 0.5',r'T = 1',r'T = 2']
+    df.drop(["row_num"], axis = 1, inplace = True)
+    ax = df.plot(kind='kde')
+    plt.xlabel= ("Profit and Loss")
+    plt.show()
+    
+
+    df2 = PLt05_2.merge(PLt2_2, how = 'left', on = 'row_num').merge(PL20_2, how = 'left', on = 'row_num')
+    df2.columns = ["row_num",r'T = 0.5',r'T = 1',r'T = 2']
+    df2.drop(["row_num"], axis = 1, inplace = True)
+    ax2 = df2.plot(kind = 'kde')
+    plt.xlabel = ("Profit and Loss")
+    plt.show()
+    '''
+
+    Tt05 = KDE_time(exercise_boundaryt05, St05, N)
+    Tt2 = KDE_time(exercise_boundaryt2, St2, N)
+    df = Tt05.merge(T20,how ='left', on = 'row_num').merge(Tt2,how ='left', on = 'row_num')
+    df.columns = ["row_num",r'T = 0.5',r'T = 1',r'T = 2']
+    df.drop(["row_num"], axis = 1, inplace = True)
+    ax = df.plot(kind='kde')
+    plt.xlabel = ("Time")
+    plt.show()
+
+  
+    
+    '''
+    #Parameter S0 9,11
+    crrs05, exercise_boundarys05 = CRRPricer_A(T, 9, mu, sigma, r, N, K)
+    crrs15, exercise_boundarys15 = CRRPricer_A(T, 11, mu, sigma, r, N, K)
+    Ss05 = pricePaths(T, 9, K, mu, sigma, r, N, paths)
+    Ss15 = pricePaths(T, 11, K, mu, sigma, r, N, paths)
+
+    PLs05, PLs05_2 = KDE_profit(crrs05, exercise_boundarys05, Ss05, K, r, dt)
+    PLs15, PLs15_2 = KDE_profit(crrs15, exercise_boundarys15, Ss15, K, r, dt)
+
+    df = PLs05.merge(PL20,how ='left', on = 'row_num').merge(PLs15,how ='left', on = 'row_num')
+    df.columns = ["row_num",r'S_0 = 9',r'S_0 = 10',r'S_0 = 11']
+    df.drop(["row_num"], axis = 1, inplace = True)
+    ax = df.plot(kind='kde')
+    plt.xlabel("Profit and Loss")
+    plt.show()
+
+    df2 = PLs05_2.merge(PL20_2,how ='left', on = 'row_num').merge(PLs15_2,how ='left', on = 'row_num')
+    df2.columns = ["row_num",r'S_0 = 9',r'S_0 = 10',r'S_0 = 11']
+    df2.drop(["row_num"], axis = 1, inplace = True)
+    ax2 = df2.plot(kind='kde')
+    plt.xlabel("Profit and Loss")
+    plt.show()
+
+    
+    Ts05 = KDE_time(exercise_boundarys05, Ss05, N)
+    Ts15 = KDE_time(exercise_boundarys15, Ss15, N)
+    df = Ts05.merge(T20,how ='left', on = 'row_num').merge(Ts15,how ='left', on = 'row_num')
+    df.columns = ["row_num",r'S_0 = 9',r'S_0 = 10',r'S_0 = 11']
+    df.drop(["row_num"], axis = 1, inplace = True)
+    ax = df.plot(kind='kde')
+    plt.xlabel("Time")
+    plt.show()
+
+    
+    #Parameter T 0.5, 2
+    crrt05, exercise_boundaryt05 = CRRPricer_A(0.5, S0, mu, sigma, r, N, K)
+    crrt2, exercise_boundaryt2 = CRRPricer_A(2, S0, mu, sigma, r, N, K)
+    St05 = pricePaths(0.5, S0, K, mu, sigma, r, N, paths)
+    St2 = pricePaths(2, S0, K, mu, sigma, r, N, paths)
+    
+    PLt05, PLt05_2 = KDE_profit(crrt05, exercise_boundaryt05, St05, K, r, 0.5/N)
+    PLt2, PLt2_2 = KDE_profit(crrt2, exercise_boundaryt2, St2, K, r, 0.5/N)
+
+    
+    df = PLt05.merge(PL20,how ='left', on = 'row_num').merge(PLt2,how ='left', on = 'row_num')
+    df.columns = ["row_num",r'T = 0.5',r'T = 1',r'T = 2']
+    df.drop(["row_num"], axis = 1, inplace = True)
+    ax = df.plot(kind='kde')
+    plt.xlabel= ("Profit and Loss")
+    plt.show()
+    
+
+    df2 = PLt05_2.merge(PLt2_2, how = 'left', on = 'row_num').merge(PL20_2, how = 'left', on = 'row_num')
+    df2.columns = ["row_num",r'T = 0.5',r'T = 1',r'T = 2']
+    df2.drop(["row_num"], axis = 1, inplace = True)
+    ax2 = df2.plot(kind = 'kde')
+    plt.xlabel = ("Profit and Loss")
+    plt.show()
+
+
+    Tt05 = KDE_time(exercise_boundaryt05, St05, N)
+    Tt2 = KDE_time(exercise_boundaryt2, St2, N)
+    df = Tt05.merge(T20,how ='left', on = 'row_num').merge(Tt2,how ='left', on = 'row_num')
+    df.columns = ["row_num",r'T = 0.5',r'T = 1',r'T = 2']
+    df.drop(["row_num"], axis = 1, inplace = True)
+    ax = df.plot(kind='kde')
+    plt.xlabel = ("Time")
+    plt.show()
+
+  
     T = PL20_2.drop(columns=["row_num"])
     T.columns = [r'$\sigma$ =0.2']
     ax = T.plot(kind='kde')
-    plt.xlabel("Time")
+    plt.xlabel = ("Profit and Loss")
     plt.show()
-     """
-
-    #Parameter mu 0.02, 0.08
+    
+   
+    #Parameter mu 0.01, 0.02
     crrmu1, exercise_boundarymu1 = CRRPricer_A(T, S0, 0.01, sigma, r, N, K)
     crrmu2, exercise_boundarymu2 = CRRPricer_A(T, S0, 0.02, sigma, r, N, K)
     Smu1 = pricePaths(T, S0, K, 0.01, sigma, r, N, paths)
@@ -34,7 +141,7 @@ if __name__ == "__main__":
     PLmu1, PLmu1_2 = KDE_profit(crrmu1, exercise_boundarymu1, Smu1, K, r, dt)
     PLmu2, PLmu2_2 = KDE_profit(crrmu2, exercise_boundarymu2, Smu2, K, r, dt)
 
-    
+
     df = PLmu1.merge(PLmu2,how ='left', on = 'row_num').merge(PL20,how ='left', on = 'row_num')
     df.columns = ["row_num",r'$\mu$ =0.01',r'$\mu$ =0.02',r'$\mu$ =0.05']
     df.drop(["row_num"], axis = 1, inplace = True)
@@ -50,16 +157,16 @@ if __name__ == "__main__":
     plt.xlabel = ("Profit and Loss")
     plt.show()
 
-    '''
+
     Tmu1 = KDE_time(exercise_boundarymu1, Smu1, N)
     Tmu2 = KDE_time(exercise_boundarymu2, Smu2, N)
     df = Tmu1.merge(Tmu2,how ='left', on = 'row_num').merge(T20,how ='left', on = 'row_num')
     df.columns = ["row_num",r'$\mu$ =0.01',r'$\mu$ =0.02',r'$\mu$ =0.05']
     df.drop(["row_num"], axis = 1, inplace = True)
     ax = df.plot(kind='kde')
-    plt.xlabel("Time")
+    plt.xlabel = ("Time")
     plt.show()
-    '''
+
 
     #Parameter sigma 0.1,0.3
     crrv1, exercise_boundaryv1 = CRRPricer_A(T, S0, mu, 0.1, r, N, K)
@@ -67,28 +174,24 @@ if __name__ == "__main__":
     Sv1 = pricePaths(T, S0, K, mu, 0.1, r, N, paths)
     Sv3 = pricePaths(T, S0, K, mu, 0.3, r, N, paths)
 
-    
     PLv1, PLv1_2 = KDE_profit(crrv1, exercise_boundaryv1, Sv1, K, r, dt)
     PLv3, PLv3_2 = KDE_profit(crrv3, exercise_boundaryv3, Sv3, K, r, dt)
 
-    '''
     df = PLv1.merge(PL20,how ='left', on = 'row_num').merge(PLv3,how ='left', on = 'row_num')
     df.columns = ["row_num",r'$\sigma$ =0.1',r'$\sigma$ =0.2',r'$\sigma$ =0.3']
     df.drop(["row_num"], axis = 1, inplace = True)
     ax = df.plot(kind='kde')
     plt.xlabel("Profit and Loss")
     plt.show()
-    '''
 
     df2 = PLv1_2.merge(PL20_2,how ='left', on = 'row_num').merge(PLv3_2,how ='left', on = 'row_num')
     df2.columns = ["row_num",r'$\sigma$ =0.1',r'$\sigma$ =0.2',r'$\sigma$ =0.3']
     df2.drop(["row_num"], axis = 1, inplace = True)
     ax2 = df2.plot(kind='kde')
-    plt.xlabel("Profit and Loss")
+    plt.xlabel = ("Profit and Loss")
     plt.show()
 
     
-    '''
     Tv1 = KDE_time(exercise_boundaryv1, Sv1, N)
     Tv3 = KDE_time(exercise_boundaryv3, Sv3, N)
     df = Tv1.merge(T20,how ='left', on = 'row_num').merge(Tv3,how ='left', on = 'row_num')
@@ -97,7 +200,7 @@ if __name__ == "__main__":
     ax = df.plot(kind='kde')
     plt.xlabel("Time")
     plt.show()
-    '''
+    
 
     #Parameter r 0.05,0.08
     crrr5, exercise_boundaryr5 = CRRPricer_A(T, S0, mu, sigma, 0.05, N, K)
@@ -108,29 +211,29 @@ if __name__ == "__main__":
     PLr5, PLr5_2 = KDE_profit(crrr5, exercise_boundaryr5, Sr5, K, r, dt)
     PLr8, PLr8_2 = KDE_profit(crrr8, exercise_boundaryr8, Sr8, K, r, dt)
 
-    '''
+
     df = PL20.merge(PLr5,how ='left', on = 'row_num').merge(PLr8,how ='left', on = 'row_num')
     df.columns = ["row_num",r'r =0.02',r'r =0.05',r'r =0.08']
     df.drop(["row_num"], axis = 1, inplace = True)
     ax = df.plot(kind='kde')
     plt.xlabel("Profit and Loss")
     plt.show()
-    '''
+    
     df2 = PL20.merge(PLr5_2,how ='left', on = 'row_num').merge(PLr8_2,how ='left', on = 'row_num')
     df2.columns = ["row_num",r'r =0.02',r'r =0.05',r'r =0.08']
     df2.drop(["row_num"], axis = 1, inplace = True)
     ax2 = df2.plot(kind = 'kde')
-    plt.xlabel("Profit and Loss")
+    plt.xlabel = ("Profit and Loss")
     plt.show()
     
-    '''
+    
     Tr5 = KDE_time(exercise_boundaryr5, Sr5, N)
     Tr8 = KDE_time(exercise_boundaryr8, Sr8, N)
     df = T20.merge(Tr5,how ='left', on = 'row_num').merge(Tr8,how ='left', on = 'row_num')
     df.columns = ["row_num",r'r =0.02',r'r =0.05',r'r =0.08']
     df.drop(["row_num"], axis = 1, inplace = True)
     ax = df.plot(kind='kde')
-    plt.xlabel("Time")
+    plt.xlabel = ("Time")
     plt.show()
     '''
 
@@ -145,7 +248,7 @@ if __name__ == "__main__":
     PL = PL20.drop(columns=["row_num"])
     PL.columns = [r'$\sigma$ =0.2']
     ax = PL.plot(kind='kde')
-    plt.xlabel("Profit and Loss")
+    plt.xlabel = ("Profit and Loss")
     plt.show()
 
     #Stimulate price paths for various realized volatility 
@@ -165,7 +268,7 @@ if __name__ == "__main__":
     df.drop(["row_num"], axis = 1, inplace = True)
     print(df)
     ax = df.plot(kind='kde')
-    plt.xlabel("Profit and Loss")
+    plt.xlabel = ("Profit and Loss")
     plt.show()
 
 '''
